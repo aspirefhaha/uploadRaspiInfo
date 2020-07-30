3322ce0feeb216158dee95c7d6e74c2152e2eb54
fhaha <aspirefhaha@gmail.com>
2020-07-30 09:03:53 +0800

    add time to comments

--- upip.sh
+++ upip.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+time=$(date "+%Y%m%d%H%M%S")
+echo ${time} >/tmp/upmytime
 local_ip=`ip addr | grep inet | grep -v 127.0.0.1 | grep -v inet6 | grep -v secondary | awk '{print $2}' | tr -d "addr:" | cut -f1  -d'/'`
 echo ${local_ip}>/tmp/localip
 my_id=`cat /home/pi/upmyself/my.id`
 echo ${my_id}>/tmp/localid
-curl -d "piid=${my_id}&piip=${local_ip}&pic=a" "fhaha.cn/polls/" >/tmp/curlres
+curl -d "piid=${my_id}&piip=${local_ip}&pic=${time}" "fhaha.cn/polls/" >/tmp/curlres
